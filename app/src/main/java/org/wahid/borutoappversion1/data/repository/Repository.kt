97a186d3976ee3ba3636b2eb515733d5b9a158ab
Wahid Ali <1213089@dtu.edu.eg>
2025-03-23 04:06:58 +0200

    Add RemoteDataSource

--- app/src/main/java/org/wahid/borutoappversion1/data/repository/Repository.kt
+++ app/src/main/java/org/wahid/borutoappversion1/data/repository/Repository.kt
@@ -1,11 +1,15 @@
 package org.wahid.borutoappversion1.data.repository
 
+import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
+import org.wahid.borutoappversion1.domain.model.Hero
 import org.wahid.borutoappversion1.domain.repository.DatastoreOperations
+import org.wahid.borutoappversion1.domain.repository.RemoteDataSource
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val datastoreOperations: DatastoreOperations
+    private val remoteDataSource: RemoteDataSource,
+    private val datastoreOperations: DatastoreOperations,
 ) {
     suspend fun saveOnBoardingState(isCompleted: Boolean) {
         return datastoreOperations.saveOnBoardingState(isCompleted = isCompleted)
@@ -14,6 +18,8 @@ class Repository @Inject constructor(
     fun readOnBoardingState(): Flow<Boolean> {
         return datastoreOperations.readOnBoardingState()
     }
+
+    suspend fun getAllHeroes(): Flow<PagingData<Hero>> = remoteDataSource.getAllHeroes()
 
 
 }
