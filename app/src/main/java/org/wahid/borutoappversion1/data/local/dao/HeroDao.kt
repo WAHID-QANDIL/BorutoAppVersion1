bba90800857f6061ff612c2a4641a8d9db6044c3
Wahid Ali <1213089@dtu.edu.eg>
2025-03-26 08:29:43 +0200

    Add getAllHeroesSync for logging

--- app/src/main/java/org/wahid/borutoappversion1/data/local/dao/HeroDao.kt
+++ app/src/main/java/org/wahid/borutoappversion1/data/local/dao/HeroDao.kt
@@ -12,18 +12,21 @@ import org.wahid.borutoappversion1.domain.model.Hero
 interface HeroDao {
 
     @Query("SELECT * FROM hero_table ORDER BY id ASC")
-    fun getAllHeroes():PagingSource<Int,Hero>
+    fun getAllHeroes(): PagingSource<Int, Hero>
 
 
     @Query("SELECT * FROM hero_table WHERE id=:heroId")
-    fun getHeroById(heroId:Int):Hero
+    fun getHeroById(heroId: Int): Hero
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addHeroes(heroes:List<Hero>)
+    suspend fun addHeroes(heroes: List<Hero>)
 
 
     @Query("DELETE FROM hero_table")
     suspend fun deleteAllHeroes()
+
+    @Query("SELECT * FROM hero_table")
+    suspend fun getAllHeroesSync(): List<Hero>
 
 
 }
